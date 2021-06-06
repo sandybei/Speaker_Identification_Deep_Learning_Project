c2b1c4b17b7ba340aa9f615497e58cd021464058
Sandy <sbei@innovation-res.eu>
2021-06-07 02:25:19 +0300

    added function for resizing audio

--- preprocess_audio.py
+++ preprocess_audio.py
@@ -1,22 +1,28 @@
 
 from scipy.io import wavfile 
 import matplotlib.pyplot as plt
-from scipy import signal
 from scipy.io import wavfile
 from pyAudioAnalysis import audioBasicIO as aIO 
 import numpy as np
 import librosa
+from scipy.signal.spectral import spectrogram
 from create_datasets import audio_signals_dict, sampling_rates_dict
 from pyAudioAnalysis.audioBasicIO import stereo_to_mono, read_audio_file
 
 
-
-def get_spectogram(sample_rate, audio_signal):
+def get_maximum_duration():
     """
-    get spectogram of an audio file
+    return maximum duration in seconds of all audio files in the training set
+    to get the duration in seconds, we divide the number of samples by sampling rate
     """
-    frequencies, times, spectrogram = audio_signal.spectrogram(audio_signal, sample_rate)
-    return spectrogram
+    durations = []
+    for key in audio_signals_dict.keys():
+        for audio_signal in audio_signals_dict[key]:
+            duration = audio_signal.shape[0] / float(sampling_rates_dict[key])
+            durations.append(duration)
+    durations = np.asarray(durations)
+    max_duration = durations.max()
+    return max_duration
 
 
 def show_spectogram(spectogram):
@@ -29,33 +35,28 @@ def show_spectogram(spectogram):
     plt.show()
 
 
-def get_maximum_duration():
+def audio_resize(sample_rate, audio_signal, max_duration):
     """
-    return maximum duration in seconds of all audio files in training set
-    to get the duration in seconds, we divide the number of samples by sampling rate
+    resizes audio signal to have the maximum signal length
     """
-    durations = []
-    for key in audio_signals_dict.keys():
-        for audio_signal in audio_signals_dict[key]:
-            duration = audio_signal.shape[0] / float(sampling_rates_dict[key])
-            durations.append(duration)
-    durations = np.asarray(durations)
-    max_duration = durations.max()
-    min_duration = durations.min()
-    return max_duration, min_duration
-
-
-def audio_trim(sample_rate, audio_signal):
-    signal_length = audio_signal.shape[0] / sample_rate
-    if signal_length > 10:
-        audio_signal = audio_signal[:10]        
+    signal_length = audio_signal.shape[0] 
+    print('audio duration :', signal_length / float(sample_rate))
+    max_length = sample_rate * max_duration
+    if signal_length > max_length:
+        audio_signal = audio_signal[:max_length]  
+    elif signal_length < max_length:
+        pad_length = max_length - signal_length
+        padding = np.zeros(int(pad_length))
+        audio_signal = np.append(audio_signal, padding)
+        signal_length = audio_signal.shape[0] / sample_rate
+        print('padded audio duration: ', audio_signal.shape[0] / float(sample_rate))        
     return audio_signal, sample_rate
 
 
 
 if __name__ == "__main__":
     
-    # all have same length
+    # all have same sampling rate / same #channels (mono)
     '''
     print(sampling_rates_dict)
     for key in sampling_rates_dict.keys():
@@ -63,17 +64,21 @@ if __name__ == "__main__":
     '''
     
     # get maximum length of audio signal
-    max_length, min_length = get_maximum_duration()
-    print(max_length, min_length)
-    print(round((max_length + min_length)/ 2, 2))
+    max_length = get_maximum_duration()
+    print('The max length of audio files is: ', max_length)
 
-    audio = audio_signals_dict['id10650'][1]
-    print(audio)
-    sr = sampling_rates_dict['id10650']
-    print(audio_trim(sr, audio))
-
-    # convert audio to mono (if stereo) (all were mono)
+    # get an audio file
+    audio = audio_signals_dict['id10650'][0]
+    sample_rate = sampling_rates_dict['id10650']
+    
+    # convert audio to mono (if stereo) 
     mono_signal = stereo_to_mono(audio)
+
+    # resize audio
+    resized_audio = audio_resize(sample_rate, mono_signal, max_length)
+
+    # get spectogram of audio in mel-scale
+    
     
     
 
