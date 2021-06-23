0c84ce30d2a5914f00ca6e25faaccca37e33b176
Sandy <sbei@innovation-res.eu>
2021-06-24 00:21:00 +0300

    added batchnirmalisation and dropout layers to the model

--- cnn.py
+++ cnn.py
@@ -1,6 +1,7 @@
 from tensorflow.keras import Sequential
 from tensorflow.keras import layers
 from tensorflow.keras.preprocessing import image_dataset_from_directory
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator 
 from tensorflow.keras.callbacks import EarlyStopping
 import tensorflow as tf
 import pandas as pd
@@ -11,28 +12,32 @@ import os
 train_dir = os.path.join('data', 'train')
 val_dir = os.path.join('data', 'val')
 
-# dataset loader parameters
+# dataset parameters
 batch_size = 64
 img_height = 128
 img_width = 147
+n_channels = 3
+
 
 # load training dataset
+train_ds = ImageDataGenerator(rescale=1./255)
 train_ds = image_dataset_from_directory(
     train_dir,
     labels='inferred',
     label_mode='categorical',
     image_size=(img_height,img_width),
-    seed=123,
+    seed=0,
     batch_size=batch_size
 )
 
 # load validation dataset
+val_ds = ImageDataGenerator(rescale=1./255)
 val_ds = image_dataset_from_directory(
     val_dir,
     labels='inferred',
     label_mode='categorical',
     image_size=(img_height,img_width),
-    seed=123,
+    seed=0,
     batch_size=batch_size
 )
 
@@ -40,17 +45,22 @@ AUTOTUNE = tf.data.AUTOTUNE
 train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
+
 # build model
 model = Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
+  layers.Input(shape=(img_height, img_width, n_channels)),
+  layers.Conv2D(16, (3,3), padding='same', activation='relu'),
+  layers.BatchNormalization(),
+  layers.MaxPooling2D((2,2)),
+  layers.BatchNormalization(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.BatchNormalization(),
   layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
+  layers.BatchNormalization(),
   layers.Flatten(),
-  layers.Dense(128, activation='relu'),
+  layers.Dense(300, activation='relu'),
+  layers.BatchNormalization(),
+  layers.Dropout((0.5)),
   layers.Dense(10, activation='softmax')
 ])
 
@@ -64,7 +74,7 @@ model.compile(
     metrics=['accuracy'])
 
 # fit model to data
-epochs=20
+epochs=30
 callback = EarlyStopping(monitor='loss', patience=5)
 history = model.fit(
   train_ds,
--- evaluate.py
+++ evaluate.py
@@ -1,6 +1,6 @@
 import os
 import tensorflow as tf
-from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing import image
 from keras.models import load_model
 from keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing import image_dataset_from_directory
@@ -9,27 +9,13 @@ from preprocess_files import metadata
 from glob import glob
 
 
-def make_prediction(test_img):
-    # convert image to numpy array
-    test_img = test_img.resize((128, 147))
-    test_img = img_to_array(test_img)
-    test_img = test_img.reshape(-1,128, 147,3)
+def prepare_image(file):
+    img = image.load_img(file, target_size=(img_height, img_width))
+    img = image.img_to_array(img)
+    img = img / 255
+    img = np.expand_dims(img, axis=0)
+    return img
 
-    # get probability
-    folders = glob("data/test/*") 
-    class_names = [os.path.basename(folder) for folder in folders]
-    class_names = sorted(class_names) 
-    y_prob = model.predict(test_img)
-    y_class = np.argmax(y_prob, axis=-1)
-    y_prob_max = np.max(y_prob)
-    prob = round(y_prob_max,2) * 100
-
-    # get predicted class
-    y_class = y_class[0]
-    id = class_names[y_class]
-    speaker = metadata.loc[metadata['VoxCeleb1 ID'] == id]
-    speaker = speaker['VGGFace1 ID'].item()
-    return speaker, prob
 
 # get datasets directories
 test_dir = os.path.join('data', 'test')
@@ -60,15 +46,31 @@ print('Test loss:', test_loss)
 print(f'Test accuracy: {test_accuracy} %\n')
 
 # make predictions
-filepath_1 = test_dir + os.sep + 'id10715' + os.sep + '41.png' 
-filepath_2 = test_dir + os.sep + 'id10397' + os.sep + '3.png'
-filepath_3 = test_dir + os.sep + 'id10935' + os.sep + '11.png'
-filepath_4 = test_dir + os.sep + 'id10945' + os.sep + '8.png'
-filepaths = [filepath_1, filepath_2, filepath_3, filepath_4]
-for i, path in enumerate(filepaths):
-    test_img = load_img(path)
-    speaker, prob = make_prediction(test_img)
-    true_id = os.path.basename(os.path.dirname(path))
+file_1 = test_dir + os.sep + 'id10715' + os.sep + '41.png' 
+file_2 = test_dir + os.sep + 'id10397' + os.sep + '3.png'
+file_3 = test_dir + os.sep + 'id10935' + os.sep + '11.png'
+files = [file_1,file_2,file_3]
+for i, file in enumerate(files):
+    test_img = prepare_image(file)
+    # get probability
+
+    folders = glob("data/test/*") 
+    print(folders)
+    class_names = [os.path.basename(folder) for folder in folders]
+    class_names = sorted(class_names) 
+    y_prob = model.predict(test_img)
+    y_class = np.argmax(y_prob, axis=-1)
+    y_prob_max = np.max(y_prob)
+    prob = round(y_prob_max,2) * 100
+
+    # get predicted class
+    y_class = y_class[0]
+    id = class_names[y_class]
+    speaker = metadata.loc[metadata['VoxCeleb1 ID'] == id]
+    speaker = speaker['VGGFace1 ID'].item()
+
+
+    true_id = os.path.basename(os.path.dirname(file))
     true_speaker = metadata.loc[metadata['VoxCeleb1 ID'] == true_id]
     true_speaker = true_speaker['VGGFace1 ID'].item()
     print('Prediction: ', i + 1)
