45837a0db0947dbbf8f76e1a9b62d76b07de818e
Sandy <sbei@innovation-res.eu>
2021-06-24 22:22:49 +0300

    added plot for gender ditribution

--- preprocess_files.py
+++ preprocess_files.py
@@ -1,17 +1,7 @@
 import os
+from numpy.lib.function_base import rot90
 import pandas as pd
 import matplotlib.pyplot as plt
-from tensorflow.keras import preprocessing
-
-def get_metadata():
-    """
-    reads voxceleb metadata file and keeps speakers names and ids
-    """
-    # load file with metadata
-    metadata = pd.read_csv(os.path.join('data', 'vox1_meta.csv'), sep='\t')
-    # keep id and name columns
-    #metadata = metadata[['VoxCeleb1 ID', 'VGGFace1 ID']]
-    return metadata
 
 
 def map_files_to_labels(dir):
@@ -46,32 +36,25 @@ dev_dir = 'data' + os.sep + 'voxceleb_data' + os.sep + 'wav'
 dev_files = create_file_dictionary(dev_dir)
 
 # get number of audio files per speaker id
-files_sum = pd.Series(dtype=float)
+files = pd.Series(dtype=float)
 new_dict = {}
 ids = dev_files.keys()
 for id in ids:
-    files_sum[id] = len(dev_files[id])
+    files[id] = len(dev_files[id])
 # select 10 speakers for classification
-files_sum = files_sum.sort_values(ascending=False)
-files_sum.iloc[:20].plot.barh(title='Number of files per speaker\n(for top 20 speakers with most audio files)')
+files = files.sort_values(ascending=False)
+files.iloc[:20].plot.barh(title='Number of files per speaker\n(for top 20 speakers with most audio files)')
 plt.savefig(os.path.join('results', 'files_per_speaker.png'))
 plt.close()
-# keep speakers with similar number of files
-files_sum = files_sum.iloc[4:14] 
-ids = files_sum.index.to_list()
-
-# get classification labels
-metadata = get_metadata()
-labels = metadata.loc[metadata['VoxCeleb1 ID'].isin(ids)]
-labels.reset_index(drop=True, inplace=True)
+files_to_keep = files.iloc[4:14] 
+ids = files_to_keep.index.to_list()
 
 # get number of files for training, validation and test set for a 80/10/10 split
-total_files = files_sum.sum()
+total_files = files_to_keep.sum()
 test_files_num = int(total_files * 10 / 100)
 val_files_num = int(total_files * 10 / 100)
 train_files_num = int(total_files - (test_files_num + val_files_num))
-files_per_id = round(test_files_num / files_sum.shape[0])
-files_sum = files_sum.iloc[:files_per_id] 
+files_per_id = round(test_files_num / files.shape[0])
 # get file info plot
 files_info = pd.Series({'Total': total_files, 'Training': train_files_num, 'Validation': val_files_num, 'Test': test_files_num})
 files_info.plot.barh(title='Number of audio files')
@@ -79,11 +62,21 @@ plt.tight_layout()
 plt.savefig(os.path.join('results', 'files_info.png'))
 plt.close()
 
+# get classification labels
+metadata = pd.read_csv(os.path.join('data', 'vox1_meta.csv'), sep='\t')
+labels = metadata.loc[metadata['VoxCeleb1 ID'].isin(ids)]
+labels.reset_index(drop=True, inplace=True)
+labels = labels.groupby(['Gender']).size()
+fig = labels.plot.bar()
+# get gender distribution plot
+plt.title('Gender Distribution')
+plt.savefig(os.path.join('results', 'genders.png'))
+plt.close()
 
 # get all audio files to be used 
 files_dict = {id: dev_files[id] for id in ids}
 
-# get training, validation and test set files
+# create training, validation and test set files from voxceleb data
 train_files = {}
 val_files = {}
 test_files = {}
